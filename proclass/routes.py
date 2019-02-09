b785268c98066e194d7621b07850dd30a14e4561
Di Wu <diw26@pitt.edu>
2019-02-08 23:50:03 -0500

    hold

--- proclass/routes.py
+++ proclass/routes.py
@@ -1,6 +1,26 @@
 from proclass import app
-from flask import render_template
+from flask import render_template, request, url_for
+from proclass.models import User
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.quest.filter_by(email=email).first()
+        if user and user.password == password:
+            # login succeed
+            session['email'] = email
+            return redirect(url_for('home'))
+
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('home'))
